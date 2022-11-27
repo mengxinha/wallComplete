66bf253f7104e853656feca538a7011db0e78e8e
mengxin <mengxinha_2021@qq.com>
2022-11-27 11:11:18 +0800

    表白墙-前后端交互

--- src/main/java/MessageServlet.java
+++ src/main/java/MessageServlet.java
@@ -2,6 +2,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.rmi.CORBA.ValueHandler;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +10,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@WebServlet("/message")
 public class MessageServlet extends HttpServlet {
 
     private ObjectMapper objectMapper = new ObjectMapper();
@@ -17,6 +19,8 @@ public class MessageServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json;charset=utf8");
+        resp.getWriter().write(objectMapper.writeValueAsString(messageList));
 
     }
 
@@ -26,6 +30,10 @@ public class MessageServlet extends HttpServlet {
         Message message = objectMapper.readValue(req.getInputStream(),Message.class);
         messageList.add(message);
         resp.setStatus(200);
-        System.out.println("提交数据成功");
+
+        System.out.println("提交数据成功: from="+message.getFrom()+
+                ",to="+message.getTo() + ",message="+message.getMessage());
+
+
     }
 }
